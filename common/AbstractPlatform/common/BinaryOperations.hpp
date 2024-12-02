54904253084d80342aa894d0d1bbec3bacd71d5c
Dmitry Krasnyansky <k.dimonych@gmail.com>
2024-12-03 00:37:24 +0200

    implementation: BinaryOperations

---
+++ common/AbstractPlatform/common/BinaryOperations.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace AbstractPlatform
+{
+
+template < typename taValue >
+static constexpr inline taValue
+SetBit( taValue aValue, size_t aBitIndex )
+{
+    return static_cast< taValue >( aValue | ( taValue{ 1 } << aBitIndex ) );
+}
+
+template < typename taValue >
+static constexpr inline taValue
+ClearBit( taValue aValue, size_t aBitIndex )
+{
+    return static_cast< taValue >( aValue & ~( taValue{ 1 } << aBitIndex ) );
+}
+
+template < typename taValue >
+static constexpr inline taValue
+ToggleBit( taValue aValue, size_t aBitIndex )
+{
+    return static_cast< taValue >( aValue ^ ( taValue{ 1 } << aBitIndex ) );
+}
+
+template < typename taValue >
+static constexpr inline bool
+CheckBit( taValue aValue, size_t aBitIndex )
+{
+    return static_cast< bool >( ( aValue >> aBitIndex ) & taValue{ 1 } );
+}
+
+}  // namespace AbstractPlatform
---
+++ output/display/AbstractPlatform/output/display/AbstractDisplay.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <AbstractPlatform/common/Platform.hpp>
+#include <AbstractPlatform/common/ErrorCode.hpp>
+
+namespace AbstractPlatform
+{
+
+}  // namespace AbstractPlatform
