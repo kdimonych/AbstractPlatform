ae340e9ea2abe977670317596ab674a22240e509
Dmitry Krasnyansky <k.dimonych@gmail.com>
2025-05-29 00:40:13 +0300

    fix: implemented  BitRangeToMask

--- common/AbstractPlatform/common/BinaryOperations.hpp
+++ common/AbstractPlatform/common/BinaryOperations.hpp
@@ -2,55 +2,142 @@
 
 #include <AbstractPlatform/common/TypeBinaryRepresentation.hpp>
 
+#include <algorithm>
 #include <type_traits>
 
 namespace AbstractPlatform {
+/**
+ * @brief Sets a specific bit in the value.
+ * This function sets the bit at the specified index to 1.
+ *
+ * @tparam taValue The type of the value to set the bit in.
+ *                 It should be an integral type.
+ * @param aValue The value in which to set the bit.
+ * @param aBitIndex The index of the bit to set (0-based).
+ * @return constexpr taValue The value with the specified bit set.
+ * @note The aBitIndex should be less than the bit size of taValue.
+ *       Passing an index that is out of bounds may lead to undefined behavior.
+ */
 template <typename taValue>
 inline static constexpr taValue SetBit(taValue aValue, size_t aBitIndex)
 {
-  using TTargetType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TTargetType = typename SizeCompatible<taValue>::TType;
   return static_cast<taValue>(static_cast<TTargetType>(aValue) | (TTargetType{1} << aBitIndex));
 }
 
 template <typename taValue>
 inline static constexpr taValue ClearBit(taValue aValue, size_t aBitIndex)
 {
-  using TTargetType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TTargetType = typename SizeCompatible<taValue>::TType;
   return static_cast<taValue>(static_cast<TTargetType>(aValue) & ~(TTargetType{1} << aBitIndex));
 }
 
+/**
+ * @brief Toggles a specific bit in the value.
+ * This function flips the bit at the specified index from 0 to 1 or from 1 to 0.
+ *
+ * @tparam taValue The type of the value to toggle the bit in.
+ *                 It should be an integral type.
+ * @param aValue The value in which to toggle the bit.
+ * @param aBitIndex The index of the bit to toggle (0-based).
+ * @return constexpr taValue The value with the specified bit toggled.
+ * @note The aBitIndex should be less than the bit size of taValue.
+ *       Passing an index that is out of bounds may lead to undefined behavior.
+ */
 template <typename taValue>
 inline static constexpr taValue ToggleBit(taValue aValue, size_t aBitIndex)
 {
-  using TTargetType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TTargetType = typename SizeCompatible<taValue>::TType;
   return static_cast<taValue>(static_cast<TTargetType>(aValue) ^ (TTargetType{1} << aBitIndex));
 }
 
+/**
+ * @brief Checks if a specific bit is set in the value.
+ * This function checks if the bit at the specified index is set to 1.
+ *
+ * @tparam taValue The type of the value to check.
+ *                 It should be an integral type.
+ * @param aValue The value to check.
+ * @param aBitIndex The index of the bit to check (0-based).
+ * @return constexpr bool True if the bit is set, false otherwise.
+ * @note The aBitIndex should be less than the bit size of taValue.
+ *       Passing an index that is out of bounds may lead to undefined behavior.
+ */
 template <typename taValue>
 inline static constexpr bool CheckBit(taValue aValue, size_t aBitIndex)
 {
-  using TTargetType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TTargetType = typename SizeCompatible<taValue>::TType;
   return static_cast<bool>((static_cast<TTargetType>(aValue) >> aBitIndex) & TTargetType{1});
 }
 
+/**
+ * @brief Returns a value with all bits set.
+ * This function returns a value where all bits are set to 1.
+ *
+ * @tparam taValue The type of the value for which all bits are set.
+ *                 It should be an integral type.
+ * @return constexpr auto A value with all bits set.
+ */
 template <typename taValue>
 inline static constexpr auto AllBitsSet()
 {
-  using TProxyType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TProxyType = typename SizeCompatible<taValue>::TType;
   return static_cast<taValue>(std::numeric_limits<TProxyType>::max());
 }
 
+/**
+ * @brief Returns a value with all bits cleared.
+ * This function returns a value where all bits are set to 0.
+ *
+ * @tparam taValue The type of the value for which all bits are cleared.
+ *                 It should be an integral type.
+ * @return constexpr auto A value with all bits cleared.
+ */
 template <typename taValue>
 inline static constexpr auto AllBitsCleared()
 {
-  using TProxyType = typename SizeCompatible<taValue>::TType;
   static_assert(std::is_integral<taValue>::value);
+
+  using TProxyType = typename SizeCompatible<taValue>::TType;
   return static_cast<taValue>(std::numeric_limits<TProxyType>::max());
+}
+
+/**
+ * @brief Returns a mask for the specified bit range.
+ * The mask is created by setting all bits from aStartBitIndex to aEndBitIndex.
+ * @note The aStartBitIndex is inclusive, while aEndBitIndex is exclusive.
+ * @note The aStartBitIndex should be less than or equal to aEndBitIndex,
+ *
+ *
+ * @tparam taValue The type of the value for which the mask is created.
+ *                 It should be an integral type.
+ * @param aFirstBitIndex The first bit index (inclusive).
+ * @param aLastBitIndex The last bit index (inclusive).
+ * @return constexpr auto The mask for the specified bit range.
+ * @note The aStartBitIndex should be less than or equal to aLastBitIndex, and both should be within
+ *       the bit size of taValue.
+ *       Passing invalid indices may lead to undefined behavior.
+ */
+template <typename taValue>
+inline static constexpr auto BitRangeToMask(size_t aFirstBitIndex, size_t aLastBitIndex) NOEXCEPT
+{
+  static_assert(std::is_integral<taValue>::value, "taValue should be an integral type");
+
+  using TProxyType = typename SizeCompatible<taValue>::TType;
+
+  return static_cast<taValue>(AllBitsSet<TProxyType>() << aFirstBitIndex
+                                ^ (AllBitsSet<TProxyType>() << aLastBitIndex)
+                              | (TProxyType{1} << aLastBitIndex));
 }
 
 } // namespace AbstractPlatform
--- common/test/BinaryOperationsTest.cpp
+++ common/test/BinaryOperationsTest.cpp
@@ -15,21 +15,21 @@ namespace {
 }
 
 template <typename T>
-struct BinaryOperationsTest : public testing::Test
+struct BinaryOperationsGroupTest : public testing::Test
 {
   using TType = T;
 };
 
-using TBinaryOperationsTestTypes = testing::Types<std::uint8_t,
-                                                  std::uint16_t,
-                                                  std::uint32_t,
-                                                  std::uint64_t,
-                                                  std::int8_t,
-                                                  std::int16_t,
-                                                  std::int32_t,
-                                                  std::int64_t,
-                                                  std::size_t>;
-TYPED_TEST_SUITE(BinaryOperationsTest, TBinaryOperationsTestTypes);
+using TBinaryOperationsGroupTestTypes = testing::Types<std::uint8_t,
+                                                       std::uint16_t,
+                                                       std::uint32_t,
+                                                       std::uint64_t,
+                                                       std::int8_t,
+                                                       std::int16_t,
+                                                       std::int32_t,
+                                                       std::int64_t,
+                                                       std::size_t>;
+TYPED_TEST_SUITE(BinaryOperationsGroupTest, TBinaryOperationsGroupTestTypes);
 
 template <typename taT>
 inline static constexpr taT SetBitTest(const size_t aBit)
@@ -68,15 +68,17 @@ inline static constexpr bool StaticBitOperationsTest()
 }
 
 template <typename taDataType, typename taIndexType, taIndexType... taIndexes>
-inline static constexpr bool StaticOperationsTest(std::integer_sequence<taIndexType, taIndexes...>)
+inline static constexpr bool
+StaticOperationsTestLoop(std::integer_sequence<taIndexType, taIndexes...>)
 {
   return (StaticBitOperationsTest<taDataType, taIndexes>(), ...), true;
 }
 
-TYPED_TEST(BinaryOperationsTest, TestMainOperation)
+TYPED_TEST(BinaryOperationsGroupTest, MainOperation)
 {
   using TType = typename TestFixture::TType;
-  static_assert(StaticOperationsTest<TType>(std::make_index_sequence<BitSize(TType{})>{}));
+
+  static_assert(StaticOperationsTestLoop<TType>(std::make_index_sequence<BitSize(TType{})>{}));
 
   auto checkForBit = [](size_t aIndex) {
     const TType kAllSetValue     = AllBitsSet<TType>();
@@ -107,5 +109,91 @@ TYPED_TEST(BinaryOperationsTest, TestMainOperation)
   for (size_t bit = 0; bit < BitSize(TType{}); ++bit)
   {
     checkForBit(bit);
+  }
+}
+
+template <typename taDataType, size_t taFirstIndex, size_t taLastIndex>
+inline static constexpr bool StaticRangeToBitMaskTest()
+{
+  constexpr size_t kFirstIndex = std::min(taFirstIndex, taLastIndex);
+  constexpr size_t kLastIndex  = std::max(taFirstIndex, taLastIndex);
+  using TProxyType             = typename SizeCompatible<taDataType>::TType;
+
+  constexpr auto kExpectedValue = static_cast<taDataType>(
+    (AllBitsSet<TProxyType>() << kFirstIndex) ^ (AllBitsSet<TProxyType>() << kLastIndex)
+    | (TProxyType{1} << kLastIndex));
+
+  static_assert(BitRangeToMask<taDataType>(kFirstIndex, kLastIndex) == kExpectedValue,
+                "BitRangeToMask does not return expected value");
+  return true;
+}
+
+template <typename taDataType,
+          size_t taFirstIndex,
+          typename taLastIndexType,
+          taLastIndexType... taLastIndexes>
+inline static constexpr bool
+StaticRangeToBitMaskTestLoopEnd(std::integer_sequence<taLastIndexType, taLastIndexes...>)
+{
+  return (StaticRangeToBitMaskTest<taDataType, taFirstIndex, taLastIndexes>(), ...), true;
+}
+
+template <typename taDataType, typename taFirstIndexType, taFirstIndexType... taFirstIndexes>
+inline static constexpr bool
+StaticRangeToBitMaskTestLoopStart(std::integer_sequence<taFirstIndexType, taFirstIndexes...>)
+{
+  return (StaticRangeToBitMaskTestLoopEnd<taDataType, taFirstIndexes>(
+            std::make_index_sequence<BitSize(taDataType{})>{}),
+          ...),
+         true;
+}
+
+TYPED_TEST(BinaryOperationsGroupTest, RangeToBitMask)
+{
+  using TType      = typename TestFixture::TType;
+  using TProxyType = typename SizeCompatible<TType>::TType;
+
+  static_assert(
+    StaticRangeToBitMaskTestLoopStart<TType>(std::make_index_sequence<BitSize(TType{})>{}));
+
+  for (size_t endIndex = BitSize(TType{}); endIndex > 0; --endIndex)
+  {
+    for (size_t firstIndex = 0; firstIndex < endIndex; --firstIndex)
+    {
+      const size_t lastIndex     = endIndex - 1;
+      const auto   expectedValue = static_cast<TType>((AllBitsSet<TProxyType>() << firstIndex)
+                                                      ^ (AllBitsSet<TProxyType>() << lastIndex)
+                                                    | (TProxyType{1} << lastIndex));
+
+      EXPECT_EQ(BitRangeToMask<TType>(firstIndex, lastIndex), expectedValue)
+        << "BitRangeToMask does not return expected value";
+    }
+  }
+}
+
+TEST(BinaryOperationsTest, RangeToBitMaskSimple)
+{
+  {
+    using TType                  = std::uint8_t;
+    constexpr TType  kExpected   = 0b11100000;
+    constexpr size_t kFirstIndex = 5;
+    constexpr size_t kLastIndex  = 8;
+
+    static_assert(BitRangeToMask<TType>(kFirstIndex, kLastIndex) == kExpected,
+                  "BitRangeToMask does not return expected value for std::uint8_t");
+    EXPECT_EQ(BitRangeToMask<TType>(kFirstIndex, kLastIndex), kExpected)
+      << "BitRangeToMask does not return expected value";
+  }
+
+  {
+    using TType                  = std::uint16_t;
+    constexpr TType  kExpected   = 0b00011111111100000;
+    constexpr size_t kFirstIndex = 5;
+    constexpr size_t kLastIndex  = 13;
+
+    static_assert(BitRangeToMask<TType>(kFirstIndex, kLastIndex) == kExpected,
+                  "BitRangeToMask does not return expected value for std::uint8_t");
+    EXPECT_EQ(BitRangeToMask<TType>(kFirstIndex, kLastIndex), kExpected)
+      << "BitRangeToMask does not return expected value";
   }
 }
