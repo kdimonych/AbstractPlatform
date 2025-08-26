4070595abfdb399f90f77273671e3d052bdda90d
Dmitriy Krasnyansky <k.dimonych@gmail.com>
2025-08-26 14:34:22 +0300

    fix: unified GTest inclusion

--- common/AbstractPlatform/common/BitOperations.hpp
+++ common/AbstractPlatform/common/BitOperations.hpp
@@ -234,6 +234,35 @@ SetByte(taDataType aOfData, size_t aAtByteIndex, taByteType aToValue) NOEXCEPT
        & ByteMask<TProxyType>(aAtByteIndex)));
 }
 
+/**
+ * @brief Reverse the bit order of an 8-bit unsigned integer
+ *
+ * @param n The 8-bit unsigned integer to be reversed
+ * @return The 8-bit unsigned integer after reversing
+ */
+inline static constexpr uint8_t BitwiseReverse8(uint8_t aValue)
+{
+  return ((aValue & 0xf0) >> 4) | ((aValue & 0x0f) << 4) | ((aValue & 0xcc) >> 2)
+         | ((aValue & 0x33) << 2) | ((aValue & 0xaa) >> 1) | ((aValue & 0x55) << 1);
+}
+
+inline static constexpr uint16_t BitwiseReverse16(uint16_t aValue)
+{
+  return ((aValue & 0xff00) >> 8) | ((aValue & 0x00ff) << 8) | ((aValue & 0xf0f0) >> 4)
+         | ((aValue & 0x0f0f) << 4) | ((aValue & 0xcccc) >> 2) | ((aValue & 0x3333) << 2)
+         | ((aValue & 0xaaaa) >> 1) | ((aValue & 0x5555) << 1);
+}
+
+inline static constexpr uint32_t BitwiseReverse32(uint32_t aValue)
+{
+  aValue = ((aValue & 0xffff0000) >> 16) | ((aValue & 0x0000ffff) << 16);
+  aValue = ((aValue & 0xff00ff00) >> 8) | ((aValue & 0x00ff00ff) << 8);
+  aValue = ((aValue & 0xf0f0f0f0) >> 4) | ((aValue & 0x0f0f0f0f) << 4);
+  aValue = ((aValue & 0xcccccccc) >> 2) | ((aValue & 0x33333333) << 2);
+  aValue = ((aValue & 0xaaaaaaaa) >> 1) | ((aValue & 0x55555555) << 1);
+  return aValue;
+}
+
 inline static constexpr bool IsValidBlockSize(size_t aBlockSize)
 {
   return aBlockSize > 1 && IsPowerOfTwo(aBlockSize) && aBlockSize <= kPlatformLongLongSize;
